7505e5aafe078d470453365abcec4910945f64a3
repitaneo <fernandez.marcos@gmail.com>
2020-01-10 12:04:11 +0100

    AutorBean creado

--- src/main/java/com/marcos/IoC/beans/Autor/AutorBean.java
+++ src/main/java/com/marcos/IoC/beans/Autor/AutorBean.java
@@ -2,4 +2,30 @@ package com.marcos.IoC.beans.Autor;
 
 public class AutorBean {
 
+	private String nombre;
+	private int edad;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	@Override
+	public String toString() {
+		return "AutorBean [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
+	
+	
+	
+	
 }
