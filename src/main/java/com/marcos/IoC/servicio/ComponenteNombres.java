394eb915a72df2c0b3646ac775925203ce44e8ee
repitaneo <fernandez.marcos@gmail.com>
2020-01-14 10:00:08 +0100

    Variando el @Scope y los @Component @Service y @Repository y haciendo pruebas con mas rutas

--- src/main/java/com/marcos/IoC/servicio/ComponenteNombres.java
+++ src/main/java/com/marcos/IoC/servicio/ComponenteNombres.java
@@ -1,8 +1,12 @@
 package com.marcos.IoC.servicio;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
+@Scope("prototype")
 public class ComponenteNombres {
 
 	
